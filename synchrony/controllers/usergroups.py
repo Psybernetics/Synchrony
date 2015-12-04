6522ae18b7cfe2fb08431097bce17c8390251ae0
luke <luke.brooks42@gmail.com>
2015-12-04 19:10:55 +0000

    Added a settings page for individual networks Introduced the toggle_signups privilege for Administrators.

--- synchrony/controllers/usergroups.py
+++ synchrony/controllers/usergroups.py
@@ -11,6 +11,7 @@ def init_user_groups():
             deactivate
             manage_networks
             review_downloads
+            toggle_signups
         Users:
             chat
             initiate_rtc
@@ -27,7 +28,7 @@ def init_user_groups():
     # Bans happen by setting User.active to False and clearing their existing sessions.
     groups = ["Administrators", "Users"]
     admin_privs  = [ "see_all", "delete_at_will", "reset_user_pw", "modify_usergroup",
-                     "deactivate", "manage_networks", "review_downloads"] 
+                     "deactivate", "manage_networks", "review_downloads", "toggle_signups"] 
     privs        = [ "create_revision_group", "delete_revision_group", "chat",
                      "initiate_rtc", "create_revision", "retrieve_from_dht", 
                      "browse_peer_nodes", "retrieve_resource", "stream_document"]
