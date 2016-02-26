bcd3ecfa939368e4b7821256f7cdfff3fdf8d2c7
luke <luke.brooks42@gmail.com>
2016-02-26 10:17:30 +0000

    Introduces itersockets in streams.documents. Introduces an outline of how things ought to fit together in documentation

--- synchrony/streams/documents.py
+++ synchrony/streams/documents.py
@@ -1,41 +1,52 @@
 # Differential synchronisation using diff-match-patch or operational transforms
 # An awareness of events (save/connect/disconnect) in streaming
-# TODO: Maintain a buffer of the entire document, with a limit for how many
-# characters may be removed in an edit
-from cgi import escape
-from synchrony import log
+#
+# Think of allocations here in terms of dom fragments.
+from synchrony import app, log
 from synchrony.controllers.auth import auth
 from flask import session, request, Response
+
 from socketio import socketio_manage
 from socketio.packet import encode, decode
 from socketio.namespace import BaseNamespace
 from socketio.mixins import RoomsMixin, BroadcastMixin
 
 class DocumentStream(BaseNamespace, RoomsMixin, BroadcastMixin):
+    """
+    Manages the lifetime of a connection in relation to channels a user is
+    interested in.
+
+    Here a channel is a URL, a user ID or a shared string.
+
+    In Gevent-SocketIO channels/rooms look like this:
+
+        self.session = {'rooms': set(['/documents_main'])}
+    
+    "/documents" is stored in self.ns_name and corresponds to the
+    io.connect("/documents", {"resource":"stream"}); part of your client-side
+    code and"main" is the channel name.
+
+    self.join("somesite.net/page") introduces "/documents_somesite.net/page"
+    to the set.
+    """
     user        = None
-    censored    = None
     socket_type = "document"
+    channels    = {} # available types: url, uid, name
+                     # Eg. {"uid": "f50068d167fc6"}
 
     def initialize(self):
-        self.cache = []
-        # The general idea for when a user connects here is to make them aware
-        # of existing document streams.
+        """
+        Individual connections have timestamped versions of documents meaning
+        they're garbage collected when people disconnect and remaining users
+        are left with the official history.
+        """
+        self.fragments = []
+        self.documents = []
         log("init document stream")
-#        self.user = None
         self.document = None
 
     def recv_connect(self):
         self.check_auth()
-
-    def recv_message(self, data):
-        self.emit("test", data)
-        print "message"
-        print data
-        print dir(self.socket)
-        self.socket.send_packet({"test":"hello"})
-        user = auth(self.request)
-        if user:
-            print user.username
 
     def recv_json(self, data):
         self.emit("test", data)
@@ -55,13 +66,26 @@ class DocumentStream(BaseNamespace, RoomsMixin, BroadcastMixin):
         self.document = document
         self.join(document)
 
+    def on_names(self, channel):
+        self.check_auth()
+        response = []
+        channel_name = self._get_room_name(channel)
+        for session_id, socket in self.socket.server.sockets.iteritems():
+            if not "rooms" in socket.session:
+                continue
+            if room_name in socket.session["rooms"] and socket != self.socket and socket.user:
+                response.append(socket.user.jsonify())
+        self.emit({"names": response})
+
+    def on_part(self, channel):
+        self.check_auth()
+        self.leave(channel)
+
     def on_edit(self, update):
         self.check_auth()
         if not self.user:
             log("no user ")
         if self.document and self.user:
-            if self.censored:
-                update = censor(update)
             log('DocumentStream: %s "%s":%i' % (self.user.username, self.document, len(update)))
             body = {"user":self.user.username,"document":update}
             self.emit_to_room(self.document, "fragment", body)
@@ -81,9 +105,15 @@ class DocumentStream(BaseNamespace, RoomsMixin, BroadcastMixin):
                 log("Received document stream connection from %s" % user.username)
                 self.user = user
 
-def censor(data):
-    replacement = "<expletive>"
-    words = []
-    for _ in words:
-        data = data.replace(_, replacement)
-    return data
+    @property
+    def itersockets(self):
+        """
+        Return a list of other active connections in this same namespace. 
+        """
+        _ = []
+        for c in self.socket.server.sockets.values():
+            if c.connected:
+                for ns in c.active_ns.values():
+                    if ns != self and ns.ns_name == self.ns_name:
+                        _.append(ns)
+        return _
