58ff6b5e1e02e6053faf8a68885fcc5237729bf2
luke <luke.brooks42@gmail.com>
2016-02-26 10:55:15 +0000

    Introduces a broadcast method to streams.documents. Removes dependence on mixins in streams.documents.

--- synchrony/streams/documents.py
+++ synchrony/streams/documents.py
@@ -9,30 +9,29 @@ from flask import session, request, Response
 from socketio import socketio_manage
 from socketio.packet import encode, decode
 from socketio.namespace import BaseNamespace
-from socketio.mixins import RoomsMixin, BroadcastMixin
 
-class DocumentStream(BaseNamespace, RoomsMixin, BroadcastMixin):
+class DocumentStream(BaseNamespace):
     """
     Manages the lifetime of a connection in relation to channels a user is
     interested in.
 
     Here a channel is a URL, a user ID or a shared string.
 
-    In Gevent-SocketIO channels/rooms look like this:
+    Channels look like this:
 
-        self.session = {'rooms': set(['/documents_main'])}
+        self.session = {'channels': set(['/documents_main'])}
     
     "/documents" is stored in self.ns_name and corresponds to the
     io.connect("/documents", {"resource":"stream"}); part of your client-side
-    code and"main" is the channel name.
+    code and "main" is the channel name.
 
     self.join("somesite.net/page") introduces "/documents_somesite.net/page"
     to the set.
     """
     user        = None
     socket_type = "document"
-    channels    = {} # available types: url, uid, name
-                     # Eg. {"uid": "f50068d167fc6"}
+    channel     = () # available types: url, uid, name
+                     # Eg. ("uid": "f50068d167fc6")
 
     def initialize(self):
         """
@@ -40,75 +39,100 @@ class DocumentStream(BaseNamespace, RoomsMixin, BroadcastMixin):
         they're garbage collected when people disconnect and remaining users
         are left with the official history.
         """
+        log("DocumentStream init")
         self.fragments = []
         self.documents = []
-        log("init document stream")
         self.document = None
+        if 'channels' not in self.session:
+            self.session['channels'] = set()
+
+    def broadcast(self, channel, event, *args):
+        pkt = dict(type="event",
+                   name=event,
+                   args=args,
+                   endpoint=self.ns_name)
+        channel_name = self.get_channel_name(channel)
+        for sessid, socket in self.socket.server.sockets.iteritems():
+            if 'channels' not in socket.session:
+                continue
+            if channel_name in socket.session['channels'] \
+            and self.socket != socket:
+                socket.send_packet(pkt)
 
     def recv_connect(self):
         self.check_auth()
 
-    def recv_json(self, data):
-        self.emit("test", data)
-        self.emit_to_room("test", data)
-        if self.user and self.document:
-            log("Received JSON from %s on %s: %s" % (self.user.username, self.document, str(data)))
-        else:
-            log("Received JSON: %s" % str(data))
+    def join(self, channel, type="name"):
+        self.session['channels'].add(self.get_channel_name(channel))
+        self.channel = (type, channel)
 
-    def on_subscribe(self, document):
+    def leave(self, channel):
+        self.session['channels'].remove(self.get_channel_name(channel))
+
+    def get_channel_name(self, channel):
+        return self.ns_name + '_' + channel
+
+    def on_subscribe(self, channel):
         user = auth(self.request)
         if user:
             self.user = user
-            log('%s has subscribed to the document stream for "%s"' % (self.user.username, document))
+            log('%s has subscribed to the document stream for "%s"' % \
+            (self.user.username, channel))
         else:
-            log('An anonymous user has subscribed to the document stream for "%s"' % document)
-        self.document = document
-        self.join(document)
+            log('An anonymous user has subscribed to the document stream for "%s"' % \
+            channel)
+        self.document = channel
+        self.join(channel)
 
     def on_names(self, channel):
         self.check_auth()
         response = []
-        channel_name = self._get_room_name(channel)
+        channel_name = self.get_channel_name(channel)
         for session_id, socket in self.socket.server.sockets.iteritems():
             if not "rooms" in socket.session:
                 continue
-            if room_name in socket.session["rooms"] and socket != self.socket and socket.user:
+            if room_name in socket.session["channels"] \
+            and socket != self.socket and socket.user:
                 response.append(socket.user.jsonify())
         self.emit({"names": response})
 
     def on_part(self, channel):
         self.check_auth()
         self.leave(channel)
+        self.broadcast(self.channel[1], "part", self.user.jsonify())
 
     def on_edit(self, update):
         self.check_auth()
         if not self.user:
             log("no user ")
         if self.document and self.user:
-            log('DocumentStream: %s "%s":%i' % (self.user.username, self.document, len(update)))
+            log('DocumentStream: %s "%s":%i' % \
+            (self.user.username, self.document, len(update)))
             body = {"user":self.user.username,"document":update}
-            self.emit_to_room(self.document, "fragment", body)
+            self.broadcast(self.document, "fragment", body)
             # Tell the transmitter their edit is going through
 #            self.emit("document", body)
 
     def recv_disconnect(self):
         if self.user and self.document:
-            log('%s has disconnected from document stream "%s"' % (self.user.username, self.document))
+            log('%s has disconnected from document stream "%s"' % \
+            (self.user.username, self.document))
         elif self.user:
-            log('%s has disconnected from an unspecified document stream.' % self.user.username)
+            log('%s has disconnected from an unspecified document stream.' % \
+            self.user.username)
 
     def check_auth(self):
         if not self.user:
             user = auth(self.request)
             if user:
-                log("Received document stream connection from %s" % user.username)
+                log("Received document stream connection from %s" % \
+                user.username)
                 self.user = user
 
     @property
     def itersockets(self):
         """
-        Return a list of other active connections in this same namespace. 
+        Return other active connections in this same namespace. 
         """
         _ = []
         for c in self.socket.server.sockets.values():
