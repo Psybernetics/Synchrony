de52edacb55597a925bfd09132aa1905404ccf8a
luke <luke.brooks42@gmail.com>
2016-03-07 16:33:13 +0000

    Introduces RPC and the beginning of a test suite for it. Purposfully kept from being a mixin class for simplicity. Introduces models.Friend.get_status (currently untested!)

--- synchrony/tests/rpc_friend_suite.py
+++ synchrony/tests/rpc_friend_suite.py
@@ -5,11 +5,32 @@ import unittest
 from synchrony.controllers import dht
 from synchrony.tests.utils import BaseSuite
 
+class User(object):
+    def __init__(self):
+        self.username = None
+        self.uid      = None
+
+class Friend(object):
+    def __init__(self):
+        self.address  = None
+
+
 class TestSuite(BaseSuite):
 
     peer_amount = 25
 
     def add_friend(self):
+        node_id = str(self.peers[0].node.long_id)
+        addr    = "Test Network/%s/foo" % node_id
+        self.peers[0].protocol.rpc_add_friend("foo", addr)
+
+    def accept_friend_request(self):
+        pass
+
+    def reject_friend_request(self):
+        pass
+    
+    def remove_friend(self):
         pass
 
 def run():
